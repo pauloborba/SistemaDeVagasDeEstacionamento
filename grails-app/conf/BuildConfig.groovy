f1c2387e710767fad14dd254302f8a547ad9d68a
Reuel Jonathan <Reuel Jonathan>
2016-05-22 22:35:14 -0300

    Change gitignore, merge GebConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,7 +7,7 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-
+grails.server.port.http = 8070
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxProfile
@@ -13,7 +12,7 @@ environments {
    // run as “grails -Dgeb.env=chrome test-app”
    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
    chrome {
-      driver = { File file = new File("C:/Users/Gilzélia/Desktop/Lucas/2016.1/ESS/Projeto/SistemaDeVagasDeEstacionamento-master/chromedrivers/chromedriver.exe"); //configurar com o enderço correto do chromedriver.
+      driver = { File file = new File("chromedrivers/chromedriver.exe"); //configurar com o enderço correto do chromedriver.
             System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
             new ChromeDriver(); }
    }
@@ -24,13 +23,3 @@ environments {
     driver = { new FirefoxDriver() }
   }
 }
-=======
-/**
- * Created by Reuel on 03/05/2016.
- */
-import org.openqa.selenium.chrome.ChromeDriver
-
-driver = {
-    new ChromeDriver()
-}
->>>>>>> e340233f5b7422d991772fa29dd6d217bbbcf457
