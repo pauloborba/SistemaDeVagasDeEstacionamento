73fffff70f2e8f78c4aa9d15546bbeddaf9c5fbe
lperrusi <lbp@cin.ufpe.br>
2016-05-24 00:55:55 -0300

    Adição dos atributos de localização, x e y

--- grails-app/domain/Vaga.groovy
+++ grails-app/domain/Vaga.groovy
@@ -2,13 +2,17 @@
  * Created by George on 22/05/16.
  */
 class Vaga {
+
     String descricao
     String setor
     boolean ocupada
     boolean preferencial
     Usuario usuario
-
+    int x
+    int y
     static constraints = {
+        x blank: false, nullable:false
+        y blank: false, nullable:false
         descricao blank: false
         usuario nullable: true
         setor inList: ["CCEN", "CIn"]
