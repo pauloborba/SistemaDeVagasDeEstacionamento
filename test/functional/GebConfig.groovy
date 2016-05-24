3aa41443ee15812ccb4cc2dbe57781c0e8ce1661
Horácio Filho (Fofofly) <hjcf@cin.ufpe.br>
2016-05-24 01:17:56 -0300

    Update GebConfig.groovy

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,12 +1,25 @@
-
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxProfile
 
 driver = {
-    File file = new File("chromedrivers/chromedriver.exe");
-
-
+ File file = new File("C:/Users/Gilzélia/Desktop/Lucas/2016.1/ESS/Projeto/SistemaDeVagasDeEstacionamento-master/chromedrivers/chromedriver.exe"); //configurar com o enderço correto do chromedriver.
     System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
     new ChromeDriver();
-}
+} 
 
-baseUrl = "http://localhost:8070/"
+environments {
+   // run as “grails -Dgeb.env=chrome test-app”
+   // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+   chrome {
+      driver = { File file = new File("C:/Users/Gilzélia/Desktop/Lucas/2016.1/ESS/Projeto/SistemaDeVagasDeEstacionamento-master/chromedrivers/chromedriver.exe"); //configurar com o enderço correto do chromedriver.
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+            new ChromeDriver(); }
+   }
+
+   // run as “grails -Dgeb.env=chrome test-app”
+   // See: http://code.google.com/p/selenium/wiki/
+   firefox {
+    driver = { new FirefoxDriver() }
+  }
+}
