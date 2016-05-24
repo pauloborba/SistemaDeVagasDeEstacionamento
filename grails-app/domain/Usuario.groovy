dd845897c929112ea4fb7986ebea6d74381cc58a
lperrusi <lbp@cin.ufpe.br>
2016-05-24 01:26:22 -0300

    Update Usuario.groovy

--- grails-app/domain/Usuario.groovy
+++ grails-app/domain/Usuario.groovy
@@ -1,14 +1,19 @@
-/**
- * Created by George on 22/05/16.
- */
 class Usuario {
-    String nome
     String login
-    String password
+    String nome
+
+    Usuario(String login, String nome) {
+        this.login = login
+        this.nome = nome
+    }
 
     static constraints = {
         login blank: false, unique: true
         nome blank: false
-        password blank: false
+    }
+
+    @Override
+    String toString() {
+        return nome
     }
 }
