51a78a11d19dcb1ec8bfa0de4956d0a0c1b77480
horaciojcfilho <hjcf@cin.ufpe.br>
2016-05-31 02:30:32 -0300

    Prepara os testes do Cucumber

--- test/cucumber/support/env.groovy
+++ test/cucumber/support/env.groovy
@@ -1,23 +1,21 @@
 package support
 
-
 import geb.Browser
 import geb.binding.BindingUpdater
 import org.codehaus.groovy.grails.test.support.GrailsTestRequestEnvironmentInterceptor
 
 import static cucumber.api.groovy.Hooks.*
 
-Before () {
+Before() {
     bindingUpdater = new BindingUpdater(binding, new Browser())
     bindingUpdater.initialize()
-    scenarioInterceptor = new GrailsTestRequestEnvironmentInterceptor (appCtx)
-    scenarioInterceptor.init ()
+
+    scenarioInterceptor = new GrailsTestRequestEnvironmentInterceptor(appCtx)
+    scenarioInterceptor.init()
 }
 
-After () {
-    Vaga.list().each { vaga ->
-        vaga.delete(flush:true)
-    }
-    scenarioInterceptor.destroy ()
-    bindingUpdater.remove ()
+After() {
+    scenarioInterceptor.destroy()
+
+    bindingUpdater.remove()
 }
