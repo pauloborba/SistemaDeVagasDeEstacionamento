7408e727bd551b00cce764ff2e4a37acf6e73719
horaciojcfilho <hjcf@cin.ufpe.br>
2016-06-19 19:23:35 -0300

    Esforço para tentar atualizar os plugins usados para versões mais atuais
    Resolvendo problemas com o Firefox

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,9 +6,29 @@ grails.project.work.dir = "target/work"
 grails.project.target.level = 1.7
 grails.project.source.level = 1.7
 grails.server.port.http = 8070
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.project.fork = [
+        // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+        //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+        // configure settings for the test-app JVM, uses the daemon by default
+        test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+        // configure settings for the run-app JVM
+        run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        // configure settings for the run-war JVM
+        war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        // configure settings for the Console UI JVM
+        console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
+    def gebVersion = "0.13.0"
+    def seleniumVersion = "2.53.0"
+    def cucumberVersion = "1.2.0"
+    def shiroVersion = "1.2.1"
+
     // inherit Grails' default dependencies
     inherits("global") {
         // specify dependency exclusions here; for example, uncomment this to disable ehcache:
@@ -16,8 +36,7 @@ grails.project.dependency.resolution = {
     }
     log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
-    legacyResolve false
-    // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
@@ -30,36 +49,33 @@ grails.project.dependency.resolution = {
         mavenRepo "http://repository.codehaus.org"
         mavenRepo "http://download.java.net/maven/2/"
         mavenRepo "http://repository.jboss.com/maven2/"
-        mavenRepo "http://snapshots.repository.codehaus.org"
-        mavenRepo "https://repo.grails.org/grails/plugins"
-        mavenRepo "http://dl.bintray.com/alkemist/maven/"
     }
 
     dependencies {
-        compile "org.spockframework:spock-grails-support:0.7-groovy-1.8"
-
-        test "org.grails:grails-datastore-test-support:1.0-grails-2.4"
-        test "org.gebish:geb-junit4:0.9.2"
-        test "org.seleniumhq.selenium:selenium-support:2.39.0"
-        test "org.seleniumhq.selenium:selenium-chrome-driver:2.39.0"
-        test "org.seleniumhq.selenium:selenium-firefox-driver:2.39.0"
+        test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
+        test "org.gebish:geb-junit4:${gebVersion}"
+        test "org.seleniumhq.selenium:selenium-support:${seleniumVersion}"
+        test "org.seleniumhq.selenium:selenium-chrome-driver:${seleniumVersion}"
+        test "org.seleniumhq.selenium:selenium-firefox-driver:${seleniumVersion}"
     }
 
     plugins {
-        build ":tomcat:7.0.55"
+        // plugins for the build system only
+        build ":tomcat:7.0.55.3" // or ":tomcat:8.0.22"
 
+        // plugins for the compile step
         compile ":scaffolding:2.1.2"
-        compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.9.6"
+        compile ':cache:1.1.8'
+        // asset-pipeline 2.0+ requires Java 7, use version 1.9.x with Java 6
+        compile ":asset-pipeline:2.5.7"
 
-        runtime ":hibernate4:4.3.5.5"
+        // plugins needed at runtime but not for compilation
+        runtime ":hibernate4:4.3.10" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
-        runtime ":shiro:1.2.1"
+        runtime ":shiro:${shiroVersion}"
 
-        test(":cucumber:1.1.0") {
-            exclude "spock-core"
-        }
-        test ":geb:0.9.2"
+        test ":cucumber:${cucumberVersion}"
+        test ":geb:${gebVersion}"
     }
 }
