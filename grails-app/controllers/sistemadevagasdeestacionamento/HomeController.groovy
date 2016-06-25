3025bb8d77bb90b7d9ee515cdfc9d8590ef2a571
horaciojcfilho <hjcf@cin.ufpe.br>
2016-06-25 12:02:57 -0300

    Usa sintaxe mais moderna de Grails

--- grails-app/controllers/sistemadevagasdeestacionamento/HomeController.groovy
+++ grails-app/controllers/sistemadevagasdeestacionamento/HomeController.groovy
@@ -4,6 +4,6 @@ import org.apache.shiro.SecurityUtils
 
 class HomeController {
     def index() {
-        [userInstance: User.findByUsername(SecurityUtils.subject.principal)]
+        respond(User.findByUsername(SecurityUtils.subject.principal as String))
     }
 }
