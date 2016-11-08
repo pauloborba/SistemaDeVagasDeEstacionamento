a9be9bef663629b2261f376ac45bcc8d9b89bba5
Gustavo Lopes <ghls@cin.ufpe.br>
2016-11-07 22:49:24 -0300

    [#128818385] validator alterado

--- grails-app/domain/sistemadevagasdeestacionamento/Book.groovy
+++ grails-app/domain/sistemadevagasdeestacionamento/Book.groovy
@@ -9,8 +9,8 @@ class Book {
 
     static constraints = {
         parkingSpace unique: true, display: false
-        inHour range: 0..23, validator: { return it >= Calendar.getInstance().get(Calendar.HOUR_OF_DAY) }
-        outHour range: 0..23, validator: { return it >= Calendar.getInstance().get(Calendar.HOUR_OF_DAY) }
+        inHour range: 0..23
+        outHour range: 0..23
         status nullable: true
     }
 
