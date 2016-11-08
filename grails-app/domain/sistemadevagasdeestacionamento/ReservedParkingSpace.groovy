ed27670352ad1b9c7954337af44ab07b7016dc04
marciojr <marciomendescjr@gmail.com>
2016-11-08 01:05:26 -0200

    Modelo Atualizado de armazenamento das reservas feitas pelo usuáio

--- grails-app/domain/sistemadevagasdeestacionamento/ReservedParkingSpace.groovy
+++ grails-app/domain/sistemadevagasdeestacionamento/ReservedParkingSpace.groovy
@@ -2,18 +2,13 @@ package sistemadevagasdeestacionamento
 
 class ReservedParkingSpace {
 
-    Integer id
-    String description // name of the parking space
-    String information // sector of the parking space
-    boolean preferential
     Date firstDate
     Date lastDate
-    String totalTime
+    Integer totalTime
+
+    static belongsTo = [ reserved : ParkingSpace]
 
     static constraints = {
-        id nullable: false , blank: false
-        description nullable: false, blank: false, unique: false
-        information inList: ["CIn", "CCEN", "Área II"]
         totalTime blank: false
     }
 }
