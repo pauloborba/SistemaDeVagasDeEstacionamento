60f1dd3d96520f0057e79a2aba52fcd3c6994564
marciojr <marciomendescjr@gmail.com>
2016-11-08 02:34:17 -0200

    Controller recebendo o resultado da função que verifica se existe registro de reserva para o usuário e filtra caso exista ou não

--- grails-app/controllers/sistemadevagasdeestacionamento/ReservedParkingSpaceController.groovy
+++ grails-app/controllers/sistemadevagasdeestacionamento/ReservedParkingSpaceController.groovy
@@ -3,9 +3,22 @@ package sistemadevagasdeestacionamento
 class ReservedParkingSpaceController {
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
+
     def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond ReservedParkingSpace.list(params), model:[reservaInstanceCount: ReservedParkingSpace.count()]
+
+        User loggedUser = User.findByUsername(AuthHelper.instance.currentUsername)
+        def available = loggedUser.available
+
+        if(available){
+            params.max = Math.min(max ?: 10, 100)
+            respond ReservedParkingSpace.list(params), model:[reservaInstanceCount: ReservedParkingSpace.count()]
+        }else{
+            flash.message = "Não há Registro de Reserva de vagas deste usuário."
+
+            redirect(controller: "home", action: "index")
+        }
+
+
     }
 
 }
