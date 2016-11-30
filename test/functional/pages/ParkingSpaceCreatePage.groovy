d4d333b284d2c808822afbc8b0820b64ba5c58d2
marciojr <marciomendescjr@gmail.com>
2016-11-30 20:20:06 -0300

    [#128814553] Modificação e Criação de Pages para o teste de GUI

---
+++ test/functional/pages/ParkingSpaceCreatePage.groovy
@@ -0,0 +1,24 @@
+package pages
+
+import geb.Page
+import steps.InternationalizationHelper;
+
+class ParkingSpaceCreatePage extends Page{
+    static url = "/parkingSpace/create"
+
+    static at = {
+        InternationalizationHelper helper = InternationalizationHelper.instance
+
+        String parkingSpace = "ParkingSpace"
+        String pageTitle = helper.getMessage("default.create.label", parkingSpace)
+        print pageTitle
+        title ==~ pageTitle
+    }
+//#if()
+    def createParkingSpace(String description, String sector) {
+        $("input[name='description']").value(description)
+        $("select[name='sector']").value(sector)
+        $("input[name='create']").click()
+    }
+//#end
+}
---
+++ test/functional/pages/ParkingSpaceList.groovy
@@ -0,0 +1,46 @@
+package pages
+
+import geb.Page
+import sistemadevagasdeestacionamento.ParkingSpace
+import steps.InternationalizationHelper
+
+class ParkingSpaceList extends Page {
+    static url = 'parkingSpace/index'
+
+    static at = {
+        InternationalizationHelper helper = InternationalizationHelper.instance
+
+        String parkingSpace = "ParkingSpace"
+        String pageTitle = helper.getMessage("default.list.label", parkingSpace)
+        title ==~ pageTitle
+    }
+
+    //#if()
+
+    def goToCreateParkingSpacePage(){
+        $("a[class='create']").click()
+    }
+
+    def goToHomePage(){
+        $("a[class='']").click()
+    }
+
+    def bookParkingSpace(String description){
+        def parkingSpace = ParkingSpace.findByDescription(description)
+        $("h4[id='${parkingSpace.getId()}']").click()
+
+    }
+
+    def checkParkingSpace(String description, String currentUser){
+        def parkingSpace = ParkingSpace.findByDescription(description)
+        def owner = $("h4[id='${parkingSpace.id}']").text()
+
+        if(owner == currentUser){
+            return true
+        }else{
+            return false
+        }
+    }
+
+
+}
