e8971cabdae3d073a227f5e011cea50e28c4c852
Eduardo Trindade <jets@cin.ufpe.br>
2016-11-30 21:58:26 -0300

    [FINISHES #133901927] Marcar como resolvido problema reportado para vaga

--- test/cucumber/ParkingSpaceSolvedReport.feature
+++ test/cucumber/ParkingSpaceSolvedReport.feature
@@ -1,4 +1,5 @@
 Feature: Set solved parking space problem
+  # Controller
   Scenario: The user has privileges to set the problem as solved
     Given The system has stored the user "master" with preference parking spaces in the "CIn" sector
     And The user is logged in the system
@@ -13,7 +14,7 @@ Feature: Set solved parking space problem
     When The user try to set as solved the problem "irregularidade"
     Then The problem "irregularidade" is not modified
 
-
+  # Gui
   Scenario: The user has privileges to set the problem as solved web
     Given I signed up as "master" with preference for parking spaces in the "CIn" sector
     And The problem report list has the problem with title "irregularidade", sector "Área II" and description "Iluminaçao"
--- test/cucumber/steps/ParkingSpaceSolvedReportSteps.groovy
+++ test/cucumber/steps/ParkingSpaceSolvedReportSteps.groovy
@@ -7,14 +7,27 @@ import sistemadevagasdeestacionamento.*
 this.metaClass.mixin(cucumber.api.groovy.Hooks)
 this.metaClass.mixin(cucumber.api.groovy.EN)
 
-
-
-
+// extract method
 def static createProblemReport(User owner, String title, String sector, String description) {
     def problemReport = new ProblemReport([user: owner, title: title, sector: sector, description: description])
     ProblemReportController controller = new ProblemReportController()
     controller.save(problemReport)
     controller.response.reset()
+}
+
+def shouldContainProblemReport(String title, boolean should) {
+    at ProblemReportPage
+
+    assert page.containsProblemReport(title) == should
+}
+
+// Controller
+Given(~/^The system has stored the user "([^"]*)" with preference parking spaces in the "([^"]*)" sector$/) { String username, String sector ->
+    currentUsername = username
+    AuthHelper.instance.signup(username, sector)
+    def user = User.findByUsername(username)
+    assert user.username == username
+    assert user.preferredSector == sector
 }
 
 And(~/^The problem report list has the problem with title "([^"]*)", sector "([^"]*)" and description "([^"]*)"$/) { String arg1, String arg2, String arg3 ->
@@ -28,76 +41,53 @@ And(~/^The problem report list has the problem with title "([^"]*)", sector "([^
     assert problemReport.description == arg3
 }
 
-And(~/^The user "([^"]*)" has sended a report with the title "([^"]*)", sector "([^"]*)", and description "([^"]*)"$/) { String arg1, String arg2, String arg3, String arg4 ->
-    def user = User.findByUsername(arg1)
-    createProblemReport(user, arg2, arg3, arg4)
-    def problemReport = ProblemReport.findByTitle(arg2)
-    assert problemReport != null
-    assert problemReport.user == user
-    assert problemReport.title == arg2
-    assert problemReport.sector == arg3
-    assert problemReport.description == arg4
-
+And(~/^The user is logged in the system$/) { ->
+    AuthHelper.instance.login(currentUsername)
+    assert AuthHelper.instance.currentUsername == currentUsername
 }
 
-And(~/^The problem "([^"]*)" is not modified$/) { String arg1 ->
+When(~/^The user try to set as solved the problem "([^"]*)"$/) { String arg1 ->
+    def problemReport = ProblemReport.findByTitle(arg1)
+    ProblemReportController controller = new ProblemReportController()
+    assert controller.resolve(problemReport)
+}
+
+Then(~/^The problem "([^"]*)" is not modified$/) { String arg1 ->
     def problemReport = ProblemReport.findByTitle(arg1)
     assert problemReport != null
 }
+
 Then(~/^The problem "([^"]*)" is removed from parking report list$/) { String arg1 ->
     // Write code here that turns the phrase above into concrete actions
     def problemReport = ProblemReport.findByTitle(arg1)
     assert problemReport == null
 }
-Given(~/^The system has stored the user "([^"]*)" with preference parking spaces in the "([^"]*)" sector$/) { String username, String sector ->
-    currentUsername = username
 
-    AuthHelper.instance.signup(username, sector)
-
-    def user = User.findByUsername(username)
-
-    assert user.username == username
-    assert user.preferredSector == sector
-}
-And(~/^The user is logged in the system$/) { ->
-    AuthHelper.instance.login(currentUsername)
-
-    assert AuthHelper.instance.currentUsername == currentUsername
-}
+// Gui
 Given(~/^I signed up as "([^"]*)" with preference parking spaces in the "([^"]*)" sector$/) { String username, String sector ->
     waitFor { to SignUpPage }
     page.proceed(username, sector)
     waitFor { at HomePage }
     assert AuthHelper.instance.currentUsername == username
 }
-When(~/^The user try to set as solved the problem "([^"]*)"$/) { String arg1 ->
-    def problemReport = ProblemReport.findByTitle(arg1)
-    ProblemReportController controller = new ProblemReportController()
-    assert controller.resolve(problemReport)
 
-
-}
 When(~/^I go to parking report list page$/) { ->
     waitFor { at HomePage }
 
     page.goToProblemReport()
 }
 
-def shouldContainProblemReport(String title, boolean should) {
-    at ProblemReportPage
-
-    assert page.containsProblemReport(title) == should
-}
-
 And(~/^I see problem "([^"]*)" in parkin report list$/) { String title ->
     shouldContainProblemReport(title, true)
 }
+
+And(~/^I select the option to set the problem "([^"]*)" as solved$/) { String title ->
+    assert page.resolve(title)
+}
+
 Then(~/^I can not see the problem "([^"]*)" in the parking problem list$/) { String title ->
     shouldContainProblemReport(title, false)
 }
-And(~/^I see the problem "([^"]*)" continues in the parking report list$/) { String title ->
+Then(~/^I see the problem "([^"]*)" continues in the parking report list$/) { String title ->
     shouldContainProblemReport(title, true)
-}
-And(~/^I select the option to set the problem "([^"]*)" as solved$/) { String title ->
-    assert page.resolve(title)
 }
